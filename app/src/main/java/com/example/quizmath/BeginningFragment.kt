e738f49ca19efe5bd8836de9a4c1a27c6420f3a9
kane87 <dracula48lives@gmail.com>
2021-04-20 19:38:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/quizmath/BeginningFragment.kt
@@ -0,0 +1,36 @@
+package com.example.quizmath
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.quizmath.databinding.FragmentBeginningBinding
+
+
+class BeginningFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+
+        
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentBeginningBinding>(inflater,
+            R.layout.fragment_beginning,container,false)
+
+        binding.startButton.setOnClickListener { view : View ->
+            //view.findNavController().navigate(R.id.action_beginningFragment_to_mathQuizFragment)
+            Toast.makeText(context, "Start Button was pushed!", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
+
+
+    }
+
+}
